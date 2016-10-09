a95f4889a0c2d5ad640157826ec4fec36f96e541
SY <sy.ahn25@gmail.com>
2016-10-09 23:02:05 +0900

    First commit

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React, { Component, PropTypes } from 'react';
+
+const propTypes = {
+
+};
+const defaultProps = {
+
+};
+
+class Header extends Component {
+
+    render() {
+        return(
+          <div className="App-header">
+            <div>Header</div>
+          </div>
+        );
+    }
+}
+
+Header.propTypes = propTypes;
+Header.defaultProps = defaultProps;
+
+export default Header;
